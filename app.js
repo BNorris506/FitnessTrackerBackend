380e75083cbcccef99405b01316748edf4d8a177
Christopher Pettypiece <chasepettypiece@gmail.com>
2023-01-06 13:13:28 -0600

    Working on Password length

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const apiRouter = express.Router();
+const apiRouter = require("./api");
 const morgan = require("morgan");
 const cors = require("cors");
+const client = require("./db/client");
+
+client.connect();
 
 app.use(cors());
 
@@ -11,8 +14,30 @@ app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use("/api", apiRouter);
-app.use((req, res, next) => {
-  next();
+
+app.get("*", (req, res, next) => {
+  res.status(404);
+  next({
+    name: "404 - not found",
+    message: "no route found for the requested url",
+  });
+});
+
+app.use((error, req, res, next) => {
+  console.error("server error: ", error);
+  if (res.statusCode < 400) res.status(500);
+  console.log({
+    error: error.message,
+    name: error.name,
+    message: error.message,
+    table: error.table,
+  });
+  res.send({
+    error: error.message,
+    name: error.name,
+    message: error.message,
+    tabel: error.table,
+  });
 });
 
 module.exports = app;
